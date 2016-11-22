5efde424104b9d63774a5820331491fbb1686615
cedricouf <cedric.noyel@gmail.com>
2016-11-23 00:47:27 +0100

    Securise BDD queries

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var fs = require('fs');
 var io_route = require('./lib/sockets')(http);
+var helmet = require('helmet');
 
+app.use(helmet());
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(express.static(__dirname + "/public"));
+
+var expiryDate = new Date( Date.now() + 60 * 60 * 1000 );
 app.use(session({
 	secret: 'u9fdsS4d.#Zd8a/4J*',
+	name : 'sesSionId478',
+	cookie: { secure: false,
+			  httpOnly: true,
+			  expires: expiryDate },
 	resave: false,
 	saveUninitialized: true,
 }));
--- public/js/questionRapide.js
+++ public/js/questionRapide.js
@@ -1,5 +1,16 @@
+
 $(document).ready(function(){
-	$('#lancer').click(function(){alert("questionnaire lancé")});
-	$('#btnSupprimerQuestion').click(function(){alert("question supprimée")});
-	$('#oui_non').click(function(){$("#rep_A").val("Oui");$("#rep_B").val("Non")});
+
+	$('#lancer').click(function(){
+		alert("questionnaire lancé")
+	});
+
+	$('#btnSupprimerQuestion').click(function(){
+		alert("question supprimée")
+	});
+
+	$('#oui_non').click(function(){
+		$("#rep_A").val("Oui");$("#rep_B").val("Non")
+	});
+
 });
