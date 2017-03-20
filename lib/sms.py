a8da6e1545f41f1ef4b9c00fb5da9885597a1859
cedricouf <cedric.noyel@gmail.com>
2017-03-20 18:42:04 +0100

    Handle accents

--- lib/sms.py
+++ lib/sms.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import unicodedata
 import logging
 import sys
 import os
 import time
+import logging
 from gsmmodem.modem import GsmModem
 
 PORT = '/dev/ttyUSB0'
 BAUDRATE = 115200
 PIN = 1234
 
+def rm_accents(s):
+    return unicodedata.normalize('NFKD',s).encode('ascii','ignore')
+
 def handleSms(sms):
-    print(u'{0}\t{1}\t{2}\n'.format(sms.number, sms.time, sms.text))
+    print(u'{0}\t{1}\t{2}\n'.format(sms.number, sms.time, rm_accents(sms.text)))
     sys.stdout.flush()
     
 
 def main():
-    time.sleep(5)
+    # time.sleep(5)
     # logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.DEBUG)
     modem = GsmModem(PORT, BAUDRATE, smsReceivedCallbackFunc=handleSms)
     modem.smsTextMode = False
